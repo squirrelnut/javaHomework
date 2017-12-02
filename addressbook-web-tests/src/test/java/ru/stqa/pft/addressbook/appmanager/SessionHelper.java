d8fdd110b4b19fb236b61fac7fffc273626c2683
squirrelnut <squirrel_nut@inbox.ru>
2017-12-03 02:28:47 +0300

    Для вспомогательных классов делегатов создан базовый класс BaseHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -3,23 +3,18 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SessionHelper {
-    private FirefoxDriver wd;
+public class SessionHelper extends HelperBase {
+
 
     public SessionHelper(FirefoxDriver wd) {
 
-        this.wd = wd;
+        super(wd);
     }
 
     public void login(String username, String password) {
-        wd.findElement(By.id("LoginForm")).click();
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.id("LoginForm")).click();
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        type(By.id("user"), username);
+        type(By.id("pass"), password);
+        click(By.xpath("//form[@id='LoginForm']/input[3]"));
+
     }
 }
